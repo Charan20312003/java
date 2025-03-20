996c9f5b1b95b5ea452ad4da33d743e150421396
Charan20312003 <Charan.xworkz@outlook.com>
2025-03-20 23:23:21 +0530

    Add files via upload

---
+++ AmazonRunner.java
@@ -0,0 +1,27 @@
+class AmazonRunner{
+public static void main(String any[]){
+Amazon ref =new Amazon();
+ref.id=1;
+ref.name="charan";
+ref.phoneNumber=9483031846L;
+ref.password="charan@123";
+System.out.println("name is:"+ref.name);
+System.out.println("id is:"+ref.id);
+System.out.println("phoneNumber is:"+ref.phoneNumber);
+
+System.out.println("password is:"+ref.password);
+ 
+
+Amazon ref1 = new Amazon();
+ref1.id=2;
+ref1.name="cherry";
+ref1.password="cheery@!23";
+ref1.phoneNumber=98765432211L;
+System.out.println("id is:"+ref1.id);
+System.out.println("name is :"+ref1.name);
+System.out.println("password is :"+ref1.password);
+System.out.println("phonenumber is :"+ref1.phoneNumber);
+}
+}
+
+
