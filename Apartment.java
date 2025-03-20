c8b5fece1fd80d31d9bb2d1c12437b340e50d57f
Charan20312003 <Charan.xworkz@outlook.com>
2025-03-20 23:29:19 +0530

    Add files via upload

---
+++ Apartment.java
@@ -0,0 +1,11 @@
+class Apartment {
+    Apartment() {
+        System.out.println("Apartment constructor invoked");
+    }
+
+    int flatNumber;
+    String ownerName;
+    int floorNumber;
+    int numberOfRooms;
+    double rent;
+}
